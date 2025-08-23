0012befc9f6d99a492a22de79b992b207b294836
imcrisam <Imcrisam@gmail.com>
2025-08-23 22:26:13 +0000

    config to deploy github

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
+const pathGithub = '/crud_frontend_next_static';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isProd ? pathGithub : '',
+  assetPrefix: isProd ? pathGithub+'/' : '',
+
+  env: {
+    NEXT_PUBLIC_ASSET_PREFIX: isProd ? pathGithub : '',
+  },
 };
 
 export default nextConfig;
